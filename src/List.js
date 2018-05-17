41b207c40f8991e3d89a2d8cffec47b85b0f68f5
Patrick Toy <patrick.toy@performancehorizon.com>
2018-05-17 16:28:29 -0400

    WIP: implemented head, tail, cons, and toArray functions

--- src/List.js
+++ src/List.js
@@ -1,35 +1,82 @@
 // module List
 
-const List = function List(xs) {
-    if (!(this instanceof List)) return new List(xs)
+const Nil = "Nil"
+
+const List = function List(...xs) {
+    if (!(this instanceof List)) return new List(...xs)
 
     if (xs == null) throw new TypeError("Cannot create a List of 'null'")
 
-    let _head = null
-    let _tail = null
+    let _head = Nil
+    let _tail = Nil
 
-    if (xs.length > 0) {
+    if (xs.length === 1) {
         _head = xs[0]
-        _tail = new List(xs.slice(1))
+        _tail = Nil
     }
 
-    this._getHead = function () {
+    if (xs.length > 1) {
+        const t = xs.slice(1)
+
+        _head = xs[0]
+        _tail = t[0] instanceof List ? t[0] : new List(...t)
+    }
+
+    this._getHead = function() {
         return _head
     }
 
-    this._getTail = function () {
+    this._getTail = function() {
         return _tail
     }
 }
 
-const of = (...xs) => List(xs)
+List.prototype.toString = function() {
+    const h = this._getHead()
+    const t = this._getTail()
 
-const isEmpty = list => list._getHead() === null
+    if (t === Nil) return "END_LIST"
+    return `List(${h}, ${t.toString()})`
+}
 
+// List.of :: Array a -> List a
+List.of = (...xs) => List(...xs)
+
+// head :: List a -> ?a
+const head = list => list._getHead()
+
+// tail :: List a -> List a
+const tail = list => list._getTail()
+
+// toArray_ :: Array a -> List a -> Array a
+const toArray_ = (acc, list) => {
+    if (list._getTail() === Nil) return acc.concat([list._getHead()])
+    return toArray_(acc.concat([list._getHead()]), list._getTail())
+}
+
+// toArray :: List a -> Array a
+const toArray = list => toArray_([], list)
+
+// cons :: a -> List a -> List a
+const cons = (x, list) => List(x, list)
+
+// isEmpty :: List a -> boolean
+const isEmpty = list => list._getHead() === Nil
+
+// length :: List a -> number
 const length = list => {
-    if (list._getHead() === null) return 0
-    if (list._getTail() === null) return 1
+    // Empty list
+    if (list._getHead() === Nil) return 0
+
+    // Non-empty list
+    if (list._getTail() === Nil) return 1
     return 1 + length(list._getTail())
 }
 
-export default { of, isEmpty, length }
+// const reverse = list => {
+//     if (isEmpty(list)) return list
+//     reverse(list._getTail()) `concat` List(list._getHead())
+// }
+
+export default List
+export { isEmpty, head, tail, length, cons, toArray }
--- src/List.spec.js
+++ src/List.spec.js
@@ -1,14 +1,37 @@
-import List from './List'
+import { describe, expect } from "./_testRunner"
+import List, { isEmpty, length, cons, toArray } from "./List"
 
-const expect = (expected, actual) => {
-    if (expected === actual) console.log("Success!")
-    else console.error(`Failure: expected '${actual}' to equal '${expected}', but it does not!`)
-}
+describe("List", () => {
+    describe("it works with List's constructor", () => {
+        expect(List().constructor.prototype, List.of().constructor.prototype)
+    })
 
-expect(true, List.isEmpty(List.of()))
-expect(false, List.isEmpty(List.of(1)))
+    describe("it can identify an empty list", () => {
+        expect(true, isEmpty(List()))
+        expect(false, isEmpty(List(1)))
+    })
 
-expect(0, List.length(List.of()))
-expect(1, List.length(List.of(1)))
-expect(2, List.length(List.of("hello", "world")))
-expect(3, List.length(List.of("super", "cow", "powers")))
+    describe("it calculates length of fixed length lists", () => {
+        expect(0, length(List()))
+        expect(1, length(List(1)))
+        expect(2, length(List("hello", "world")))
+        expect(3, length(List("super", "cow", "powers")))
+    })
+
+    describe("it calculates length of a random length list", () => {
+        const rand = parseInt(Math.random() * 100, 10)
+        const array = Array.apply(null, new Array(rand)).fill(0)
+        expect(rand, length(List(...array)))
+    })
+
+    describe("it converts a List to a JS Array", () => {
+        expect("1,2,3", toArray(List(1, 2, 3)).toString())
+    })
+
+    describe("it can prepend a value to a List (cons || ::)", () => {
+        expect(
+            List("hello", "world", "let's", "go").toString(),
+            cons("hello", List("world", "let's", "go")).toString()
+        )
+    })
+})
